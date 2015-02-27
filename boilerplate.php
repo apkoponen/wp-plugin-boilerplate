70a3f2740e78678c2549c172662139d1d0c692da
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-02-27 22:05:44 +0100

    First commit

---
+++ boilerplate.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Plugin Name: Plugin Boilerplate
+ * Description: The description of the plugin.
+ * Author: Fredrik Forsmo
+ * Author URI: https://forsmo.me/
+ * Plugin URI: https://github.com/frozzare/wp-plugin-boilerplate
+ * Text Domain: boilerplate
+ * Version: 1.0.0
+ */
+
+// Make sure the plugin does not expose any info if called directly
+defined( 'ABSPATH' ) || exit;
+
+// Framework requires PHP 5.4 or newer
+if ( version_compare( PHP_VERSION, '5.4.0', '<' ) ) {
+  exit( 'The Plugin Boilerplate for WordPress requires PHP version 5.4 or higher.' );
+}
+
+// Load Composer autoloader.
+require 'vendor/autoload.php';
+
+/**
+ * Get the Plugin Boilerplate instance.
+ *
+ * @return \Boilerplate\PluginLoader
+ */
+
+function boilerplate() {
+  return \Boilerplate\PluginLoader::instance();
+}
+
+// Initialize on plugins loaded
+add_action( 'plugins_loaded', 'boilerplate', 0, 0 );
---
+++ src/PluginLoader.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Boilerplate;
+
+/**
+ * Hook the WordPress plugin into the appropriate WordPress actions and filters.
+ *
+ * @since 1.0.0
+ */
+
+final class PluginLoader {
+
+	/**
+	 * The instance of Plugin loader class.
+	 *
+	 * @var object
+	 * @since 1.0.0
+	 */
+
+  private static $instance;
+
+  /**
+   * Plugin boilerplate textdomain.
+   *
+   * @var string
+   * @since 1.0.0
+   */
+
+  const TEXT_DOMAIN = 'boilerplate';
+
+	/**
+	 * Papi loader instance.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return object
+	 */
+
+	public static function instance() {
+    if ( ! isset( self::$instance ) ) {
+      self::$instance = new static;
+      self::$instance->setupActions();
+      self::$instance->setupFilters();
+      self::$instance->loadTranslatedText();
+    }
+
+    return self::$instance;
+  }
+
+	/**
+	 * Load translated strings for the current locale, if a translation exists.
+	 *
+	 * @since 1.0.0
+	 */
+
+  private function loadTranslatedText() {
+    load_plugin_textdomain( self::TEXT_DOMAIN, false, dirname( dirname( __FILE__ ) ) . '/languages' );
+  }
+
+	/**
+	 * Setup WordPress actions.
+	 *
+	 * @since 1.0.0
+	 */
+
+	private function setupActions() {
+    add_action( 'init', array( __CLASS__, 'loadTranslatedText' ) );
+  }
+
+	/**
+	 * Setup WordPress filters.
+	 *
+	 * @since 1.0.0
+	 */
+
+	private function setupFilters() {}
+
+}
