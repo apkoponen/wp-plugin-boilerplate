3ed1a93938a5adfb760b525ea20cfdabab336090
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-03-01 19:39:32 +0100

    Fixing php code after coding standard

--- boilerplate.php
+++ boilerplate.php
@@ -15,7 +15,7 @@ defined('ABSPATH') || exit;
 
 // Framework requires PHP 5.4 or newer
 if (version_compare(PHP_VERSION, '5.4.0', '<')) {
-    exit('The Plugin Boilerplate for WordPress requires PHP version 5.4 or higher.');
+  exit('The Plugin Boilerplate for WordPress requires PHP version 5.4 or higher.');
 }
 
 // Load Composer autoloader.
@@ -27,9 +27,8 @@ require 'vendor/autoload.php';
  * @return \Boilerplate\PluginLoader
  */
 
-function boilerplate()
-{
-    return \Boilerplate\PluginLoader::instance();
+function boilerplate() {
+  return \Boilerplate\PluginLoader::instance();
 }
 
 // Initialize on plugins loaded
