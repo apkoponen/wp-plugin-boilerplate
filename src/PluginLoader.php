e8b0586d6d4bfa3315458968d408fa4d3a38b3ec
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-03-01 19:38:09 +0100

    Testing pre-commit

--- src/PluginLoader.php
+++ src/PluginLoader.php
@@ -53,9 +53,7 @@ final class PluginLoader {
    * @since 1.0.0
    */
 
-  public function loadTranslatedText()
-
-  {
+  public function loadTranslatedText() {
     load_plugin_textdomain(self::TEXT_DOMAIN, false, dirname(dirname(__FILE__)) . '/languages');
   }
 
