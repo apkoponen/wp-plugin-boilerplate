db818d6a19d7254fbddc4a3c6ca98be7e8bc8494
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-03-01 19:27:03 +0100

    Testing pre-commit

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -8,17 +8,19 @@ namespace Boilerplate\Tests;
  * @since 1.0.0
  */
 
-class PluginTest extends \PHPUnit_Framework_TestCase {
+class PluginTest extends \PHPUnit_Framework_TestCase
+{
 
-  /**
+    /**
    * The action `plugins_loaded` should have the `boilerplate` hook
    * and should have a default priority of 0.
    *
    * @since 1.0.0
    */
 
-  public function testPluginsLoadedAction() {
-    $this->assertEquals(0, has_action('plugins_loaded', 'boilerplate'));
-  }
+    public function testPluginsLoadedAction()
+    {
+        $this->assertEquals(0, has_action('plugins_loaded', 'boilerplate'));
+    }
 
 }
